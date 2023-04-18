035e750eb3c977589547ce0587c544358c4b5eb5
deniscyril <denis.prof@gmail.com>
2023-04-18 12:32:08 +0200

    corrections bug sur limites questions actuelles

--- quizz.py
+++ quizz.py
@@ -28,10 +28,11 @@ st.markdown(f"# Réglages")
 chap_courant = st.selectbox('Choisir le chap actuel', (k for k in sommaire.keys()))
 #st.write(chap_courant)
 #st.write(sommaire[chap_courant][1])
-st.markdown(f"Ce chapitre contient {sommaire[chap_courant][1]} questions")
-question_chap_courant_max = st.slider('Progression dans le chapitre', 0, sommaire[chap_courant][1],1)
-nb_question_courant = st.number_input('Nombre de questions à poser sur le chapitre actuel',min_value = 1,max_value=question_chap_courant_max,value=1)
-nb_question = st.number_input('Nombre de questions sur le passé',min_value = 1,max_value=question_chap_courant_max,value=1)
+st.markdown(f"Ce chapitre contient {sommaire[chap_courant][1]+1-sommaire[chap_courant][0]} questions")
+nb_question_dans_chap_courant = sommaire[chap_courant][1]+1-sommaire[chap_courant][0]
+question_chap_courant_max = st.slider('Progression dans le chapitre', 0, nb_question_dans_chap_courant,1)
+nb_question_courant = st.number_input('Nombre de questions à poser sur le chapitre actuel',min_value = 1,max_value=nb_question_dans_chap_courant,value=1)
+nb_question = st.number_input('Nombre de questions sur le passé',min_value = 1,max_value=sommaire[chap_courant][1],value=1)
 
 st.markdown("## Roulette de questions actuelles")
 # Calcul des index des questions corrspondant au début et fin du chapitre sélectionné
@@ -40,7 +41,8 @@ end_index = sommaire[chap_courant][1]
 #st.write(start_index)
 #st.write(end_index)
 # Extraction des nb_question_courant
-random_indexes_courant = random.sample(range(start_index,end_index), nb_question_courant)
+random_indexes_courant = random.sample(range(start_index,start_index+question_chap_courant_max), nb_question_courant)
+#st.write(random_indexes_courant)
 resultat_courant = df.iloc[random_indexes_courant]
 # Extraction des nb_question
 random_indexes = random.sample(range(0,end_index), nb_question)
